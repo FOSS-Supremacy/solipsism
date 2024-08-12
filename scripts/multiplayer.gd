65d98d35ce1ffd1261c81c081b87d0fc9a79d5c0
Ribbon <>
2024-08-12 03:57:42 -0300

    Add LAN multiplayer support, test scenes and internal lighting on the car

---
+++ scripts/multiplayer.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene: PackedScene
+
+func _on_host_pressed():
+	peer.create_server(6005)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player()
+
+func _add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child",player)
+
+func _on_join_pressed():
+	peer.create_client("localhost", 6005)
+	multiplayer.multiplayer_peer = peer
