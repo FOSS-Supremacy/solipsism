f00c44d0dd54eaa7e4818f2d07a652131680086d
Ribbon <>
2024-08-16 13:53:25 -0300

    Some improvements

--- scripts/music.gd
+++ scripts/music.gd
@@ -1,15 +1,9 @@
-extends Node3D
+extends AudioStreamPlayer
 
-@onready var speakers = [ $back_left_speaker, $back_right_speaker, $front_left_speaker, $front_right_speaker ]
-
-func _input(_event):
+func _input(event):
 	if Input.is_action_just_pressed("play_pause_music"):
-		for speaker in speakers:
-			speaker.playing = not speaker.playing
-			speaker.stream_paused = not speaker.stream_paused
-	if Input.is_action_just_pressed("decrease_music_volume"):
-		for speaker in speakers:
-			speaker.unit_size = speaker.unit_size - 0.2
-	if Input.is_action_just_pressed("increase_music_volume"):
-		for speaker in speakers:
-			speaker.unit_size = speaker.unit_size + 0.2
+		play()
+	if Input.is_action_pressed("decrease_music_volume"):
+		volume_db = volume_db - 2
+	if Input.is_action_pressed("increase_music_volume"):
+		volume_db = volume_db + 2
